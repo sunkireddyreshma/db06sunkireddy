f3648a50a3adacef3d95e43d36dd29d826b8c587
Reshma <sunkireddyreshma1999@gmail.com>
2021-11-17 23:05:37 -0600

    bonus code added

--- routes/horse.js
+++ routes/horse.js
@@ -3,5 +3,6 @@ const horse_controlers= require('../controllers/horse');
 var router = express.Router();
 // GET request for one horse.
 router.get('/horses/:id', horse_controlers.horse_detail);
-
+/* GET create costume page */
+router.get('/create', horse_controlers.horse_create_Page);
 module.exports = router;
